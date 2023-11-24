d1946259fa06fb8d9ef8b0c8684d0e4f029a9e71
admin <admin@szce.com>
2023-11-24 17:00:18 +0800

    快速入门案例

---
+++ boot3-01-demo/src/main/java/com/github/boot301demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.github.boot301demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String SayHello() {
+        return "Hello SpringBoot3";
+    }
+}
---
+++ boot3-01-demo/src/test/java/com/github/boot301demo/Boot301DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.boot301demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot301DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
